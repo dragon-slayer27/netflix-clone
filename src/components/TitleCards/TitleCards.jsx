d0ba3e059eac6efbc3f49ac6c96e86ce72bdf5d6
dragon-slayer27 <vivaan.teotia@gmail.com>
2024-12-28 16:14:28 +0530

    added video player

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 
+import { Link } from "react-router-dom";
+
 const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
 
@@ -40,12 +42,12 @@ const TitleCards = ({ title, category }) => {
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
               <img
                 src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
               />
-              <p>{card.original_title}</p>
-            </div>
+              <p>{card.title}</p>
+            </Link>
           );
         })}
       </div>
